e3f4b27fe898c6de1e0c76083b7e09dea667ec6b
ANUJ BISHT <anujbisht09@gmail.com>
2023-11-10 01:03:09 +0530

    Update CreateLinkedList.py

--- Main/Programs/Linked List/Singly Linked List/CreateLinkedList.py
+++ Main/Programs/Linked List/Singly Linked List/CreateLinkedList.py
@@ -1,1 +1,40 @@
 # Program to create a singly linked list
+
+# class to create a node
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+
+# class to create a linked list
+class LinkedList:
+  # function to create an empty linked list
+  def __init__(self):
+    self.head = None
+    self.tail = None
+
+  # function to insert the data into linked list
+  def insertion(self, data):
+    node = Node(data)
+    if self.tail:
+      self.tail.next = node
+      self.tail = node
+    else:
+      self.head = node
+      self.tail = node
+
+  # function to display the elements of linked list
+  def display(self):
+    current = self.head
+    while current:
+      value = current.data
+      current = current.next
+      yield value
+
+
+# driver
+if __name__ == '__main__':
+  # create an object of class LinkedList
+  items = LinkedList()
+  
